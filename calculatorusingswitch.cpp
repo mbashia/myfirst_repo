d5bf8a7ef78b53c5622b4248bd555306a6d8f173
mbashia <mbashiavictor@gmail.com>
2022-09-27 11:18:59 +0300

    calculator

---
+++ calculatorusingswitch.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int op1, op2, op;
+    cout << "enter first operand:";
+    cin >> op1;
+    cout<<"enter second operand:";
+    cin >> op2;
+    
+
+}
