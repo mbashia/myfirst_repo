d7b548f2db03fcb1444482080b96fb2d68ea6ea6
mbashia <mbashiavictor@gmail.com>
2022-09-28 21:21:57 +0300

    array_to_string

---
+++ arraytostring.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string convert(char a[90]){
+ string s=a;
+ return s;
+}
+int main(){
+    string s;
+    char pwd[90] = {'m', 'b', 'a', 's', 'h', 'i', 'a'};
+    convert(pwd);
+    cout << s;
+}
